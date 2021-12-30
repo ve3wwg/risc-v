1964b5d2e68ccb481933b0f6fb309139e2244556
Warren Gay <ve3wwg@gmail.com>
2021-12-30 15:44:36 -0500

    Fix

--- 03/add3/main/main.c
+++ 03/add3/main/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-extern int add3(int one,int two);
+extern int add3(int one,int two,int three);
 
 void
 app_main(void) {
 	int a=23, b=24, c=25;
 	int r = 0;
 	
+	r = add3(a,b,c);
 	printf("r = %d\n",r);
 }
