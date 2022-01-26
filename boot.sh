7e34e4e7e321b5a722e2ee81e8c626a018305f14
Warren Gay <ve3wwg@gmail.com>
2022-01-26 15:28:26 -0500

    Use the Developer images

--- boot.sh
+++ boot.sh
@@ -3,13 +3,13 @@
 exec /usr/local/bin/qemu-system-riscv64 \
    -nographic \
    -machine virt \
-   -smp 4 \
+   -smp 2 \
    -m 2G \
-   -kernel Fedora-Minimal-Rawhide-20200108.n.0-fw_payload-uboot-qemu-virt-smode.elf \
+   -kernel Fedora-Developer-Rawhide-20200108.n.0-fw_payload-uboot-qemu-virt-smode.elf \
    -bios none \
    -object rng-random,filename=/dev/urandom,id=rng0 \
    -device virtio-rng-device,rng=rng0 \
    -device virtio-blk-device,drive=hd0 \
-   -drive file=Fedora-Minimal-Rawhide-20200108.n.0-sda.raw,format=raw,id=hd0 \
+   -drive file=Fedora-Developer-Rawhide-20200108.n.0-sda.raw,format=raw,id=hd0 \
    -device virtio-net-device,netdev=usernet \
    -netdev user,id=usernet,hostfwd=tcp::10000-:22
