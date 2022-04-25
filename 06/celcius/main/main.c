016a4573258e71d4ae21704899855a2b671107bb
Warren Gay <ve3wwg@gmail.com>
2022-04-25 16:27:21 -0400

    initial

---
+++ 06/celcius/main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+extern void convtemp();
+
+int temp_f10 = 400;	// Fahrenheit degrees * 10
+int temp_c10 = 0;	// Computed result: Celcius * 10
+
+void
+app_main(void) {
+
+	convtemp();	// Convert temp_f10 to temp_c10
+
+	printf("%d.%d F -> %d.%d C\n",
+		temp_f10 / 10,
+		temp_f10 % 10,
+		temp_c10 / 10,
+		temp_c10 % 10);
+}
