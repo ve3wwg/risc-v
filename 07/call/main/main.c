22d0eaeb7569f1e7c9f03ea83804a9c1e325aba3
Warren Gay <ve3wwg@gmail.com>
2022-05-10 10:45:32 -0400

    New call project

---
+++ 07/call/main/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern int bigcall(
+	int32_t one,
+	int64_t two,
+	int32_t three,
+	int64_t four,
+	int32_t five,
+	int64_t six,
+	int32_t seven,
+	int64_t eight,
+	int32_t nine
+);
+
+void
+app_main(void) {
+
+	printf("Calling bigcall()..\n");
+	fflush(stdout);
+
+	int rc = bigcall(101,102,103,104,105,106,107,108,109);
+
+	printf("bigcall() returned %d\n",rc);
+	fflush(stdout);
+}
