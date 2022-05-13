952d97e5c5f1dca6b4d476b7159eedda6a048597
Warren Gay <ve3wwg@gmail.com>
2022-05-13 14:41:23 -0400

    Fix main + printf formatting

--- 08/parity/qemu64/main.c
+++ 08/parity/qemu64/main.c
@@ -5,8 +5,8 @@
 
 extern int64_t odd_parity(int64_t bits); // Assembler routine
 
-void
-app_main(void) {
+int
+main(int argc,char **argv) {
 	static int64_t tests[] = { 0, 1, 2, 3, 5, 7, 9, 15, 
 	  	31, 63, 64, 127, 1023, 1024, 2047, 2048,
 	  	65535, 65536, 999999 };
@@ -15,7 +15,7 @@ app_main(void) {
 	for ( unsigned ux=0; tests[ux] < 999999ll; ++ux ) {
 		bits = tests[ux];
 		oddpar = odd_parity(bits);
-		printf("odd_parity(%4d) (0x%04X) returned %d\n",
+		printf("odd_parity(%5d) (0x%06X) returned %d\n",
 			bits, bits, oddpar);
 	}
 	printf("Done.\n");
