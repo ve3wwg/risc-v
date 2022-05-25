1e58887df2a19327c92d9ca8cbc1d7802dfeaeb0
Warren Gay <ve3wwg@gmail.com>
2022-05-25 14:16:19 -0400

    Fix int64_t as uint64_t

--- 09/multipu/main/main.c
+++ 09/multipu/main/main.c
@@ -7,7 +7,7 @@ extern uint64_t add64(uint64_t op1,uint64_t op2);
 
 void
 app_main(void) {
-	volatile int64_t x=0x7FFFFFFFF, y=0x3000011115, z;
+	volatile uint64_t x=0x7FFFFFFFF, y=0x3000011115, z;
 
 	z = add64(x,y);
 	printf("0x%016llX + 0x%016llX = 0x%016llX\n",x,y,z);
