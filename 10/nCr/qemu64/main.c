11ed3e247499641902fd2c128a903905ea76fb21
Warren Gay <ve3wwg@gmail.com>
2022-05-31 13:44:00 -0400

    initial

---
+++ 10/nCr/qemu64/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern uint64_t nCr(int64_t n,int64_t r);
+
+struct s_test {
+	uint64_t	n;
+	uint64_t	r;
+};
+
+int
+main(int argc,char **argv) {
+	static struct s_test const tests[] = {
+		{ 3, 2 }, 	// 3
+		{ 4, 3 },	// 4
+		{ 9, 3 },	// 84
+		{ 13, 7 },	// 1716
+		{ 0, 0 }
+	};
+	
+	for ( unsigned ux=0; tests[ux] != 0; ++ux ) {
+		uint64_t ncr = nCr(tests[ux].n,tests[ux].r);
+		printf("C(n=%lu,r=%lu) => %lu\n",
+			tests[ux].n,
+			tests[ux].r,
+			ncr);
+	}
+
+	return 0;
+}
