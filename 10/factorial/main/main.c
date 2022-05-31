ecf9ad7d07cd51c1c207045a2e1de9c27977f095
Warren Gay <ve3wwg@gmail.com>
2022-05-31 13:11:35 -0400

    Result only valid up to 12!

--- 10/factorial/main/main.c
+++ 10/factorial/main/main.c
@@ -6,7 +6,7 @@ extern uint32_t ufact32(uint32_t x);
 void
 app_main(void) {
 		
-	for ( unsigned ux=0; ux<=13; ++ux ) {
+	for ( unsigned ux=0; ux<=12; ++ux ) {
 		uint32_t f = ufact32(ux);
 
 		printf("%2u ! => %u 0x%08X\n",ux,f,f);
