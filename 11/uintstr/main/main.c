0b46b3601086713f6f796b2413b6e2131469f173
Warren Gay <ve3wwg@gmail.com>
2022-06-02 11:59:41 -0400

    initial

---
+++ 11/uintstr/main/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+extern char *uintstr(unsigned v,char *buf,unsigned buflen);
+
+void
+app_main(void) {
+	static unsigned const tests[] = {
+		1023, 32, 96001, 10045, 90999, 1770771, 0, 0xFFFFFF
+	};
+	char buf[7];
+	char const *cp;
+	unsigned v;
+
+	for ( unsigned ux=0; (v = tests[ux]) != 0xFFFFFF; ++ux ) {
+		cp = uintstr(v,buf,sizeof buf);
+
+		printf("uintstr(%u,buf,%u) => '%s' %s\n",
+			v,(unsigned)sizeof buf,cp,
+			cp <= buf ? "!!" : "");
+	}
+
+	puts("Done");
+	fflush(stdout);
+}
