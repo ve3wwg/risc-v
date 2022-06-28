9043497ac5dffc577792d65bb396e300aa0f00eb
Warren Gay <ve3wwg@gmail.com>
2022-06-27 22:26:30 -0400

    initial

---
+++ 14/extend/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+extern unsigned extensions(char *buf,unsigned bufsiz,unsigned *bits);
+
+void
+app_main(void) {
+	char buf[32];
+	unsigned exten=0, bits=0;
+
+	exten = extensions(buf,sizeof buf,&bits);	
+
+	printf("exten = 0x%06X, %u bits, '%s'\n",
+		exten, bits, buf);
+}
