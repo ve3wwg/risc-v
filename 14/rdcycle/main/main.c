f473f508fd8395a94a4e17ef9aa0a0ef425e0cfd
Warren Gay <ve3wwg@gmail.com>
2022-07-08 20:50:45 -0400

    initial

---
+++ 14/rdcycle/main/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern uint32_t measure(int mul);
+
+void
+app_main(void) {
+	uint32_t cycles;
+
+	for ( int x=0; x<10; ++x ) {
+		cycles = measure(1);
+		printf("muliply cycles = %u\n",cycles);
+		cycles = measure(0);
+		printf("shift   cycles = %u\n",cycles);
+	}
+}
