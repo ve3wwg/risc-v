3bdae0f5ecc08a308450bdebdcefed69a6ef6722
Warren Gay <ve3wwg@gmail.com>
2022-07-11 21:28:06 -0400

    Updates/corrections

--- 14/rdcycle/qemu64/main.c
+++ 14/rdcycle/qemu64/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-extern uint64_t measure();
+extern uint64_t measure(int mul);
 
 int
 main(int argc,char **argv) {
 	uint64_t cycles;
 
-	cycles = measure();
-
-	printf("cycles = %lu (0x%08lX)\n",cycles,cycles);
+	for ( int x=0; x<10; ++x ) {
+		cycles = measure(1);
+		printf("muliply cycles = %lu\n",cycles);
+		cycles = measure(0);
+		printf("mul10 cycles   = %lu\n",cycles);
+	}
 	return 0;
 }
