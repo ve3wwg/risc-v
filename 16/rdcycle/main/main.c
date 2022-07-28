1e338f33c016bbe9fed97fba59dc592ec4d29b67
Warren Gay <ve3wwg@gmail.com>
2022-07-27 21:34:40 -0400

    initial

---
+++ 16/rdcycle/main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#pragma optimize GCC("-O3");
+
+extern uint32_t measure(int mul);
+
+void
+app_main(void) {
+	uint32_t cycles;
+
+	asm ( "nop" );
+
+	for ( int x=0; x<10; ++x ) {
+		cycles = measure(1);
+		printf("muliply cycles = %u\n",cycles);
+		cycles = measure(0);
+		printf("shift   cycles = %u\n",cycles);
+	}
+	fflush(stdout);
+}
