9d11ad88def2742d23c678c52e384b444b5024cb
Warren Gay <ve3wwg@gmail.com>
2022-08-02 20:56:08 -0400

    initial

---
+++ 16/inmul/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void
+app_main(void) {
+	uint32_t a=33, b=75, m;
+
+	asm volatile (
+		" lw a0,%1\n"		// a0 = a
+		" lw a1,%2\n"		// a1 = b
+		" mul t0,a0,a1\n"	// t0 = a0 * a1
+		" sw t0,%0\n"		// m = t0
+		: "=m" (m)		// Outputs
+		: "m" (a), "m" (b)	// Inputs
+		: "a0", "a1", "t0"
+	);
+
+	printf("%u * %u => %u\n",a,b,m);
+	fflush(stdout);
+}
--- 16/rdcycle/main/main.c
+++ 16/rdcycle/main/main.c
@@ -6,8 +6,17 @@ extern uint32_t measure(int mul);
 void
 app_main(void) {
 	uint32_t cycles;
+	uint32_t ninety5;
 
-	asm volatile ( "nop" );
+	asm volatile (
+		" li t1,95\n"
+		" sw t1,%0\n"
+		" sw x0,%1\n"
+		: "=m" (ninety5), "=m" (cycles)
+		:
+	);
+
+	printf("ninety5 = %u, cycles = %u\n",ninety5,cycles);
 
 	for ( int x=0; x<10; ++x ) {
 		cycles = measure(1);
